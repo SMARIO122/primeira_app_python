4afa6b8c307357d3d09c02d959c31876cfb5cb03
SeuNome <sergiolima122@gmail.com>
2025-04-15 11:30:49 -0300

    "Criar menu inicial do sistema de restaurantes"
    "Implementado menu com opções de cadastro, listagem e ativação de restaurantes, além da opção de sair. Utilizado input numérico e estrutura condicional para navegação básica."

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-print('Sabor Express\n')
+print('🆂🅰🅱🅾🆁 🅴🆇🅿🆁🅴🆂🆂')
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print('Você escolheu a opção {}'.format(opcao_escolhida))
+opcao_escolhida = int(input('Escolha uma opção: '))
+print('Você escolheu a opção {}!'.format(opcao_escolhida))
+
+if opcao_escolhida == 1:
+    print('Cadastrar Restaurante')
+elif opcao_escolhida == 2:
+    print('Listar Restaurante')
+elif opcao_escolhida == 3:
+    print('Ativar restaurantes')
+else:
+    print('Encerrando o programa')
+
+
 
 
 
