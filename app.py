54cf14dba84de5bdf03faa6ccada76bec1bf5ab1
SeuNome <sergiolima122@gmail.com>
2025-04-24 15:49:06 -0300

    "Adicionar funcionalidade de cadastro de restaurante"
    Foi implementada a opção 1 do menu principal, que permite cadastrar um novo restaurante.
    O nome digitado pelo usuário é armazenado em uma lista chamada `restaurantes` e confirmado na tela.
    Inclui também tratamento básico para opção inválida e retorno ao menu.

--- app.py
+++ app.py
@@ -1,27 +1,67 @@
 import os
-print('🆂🅰🅱🅾🆁 🅴🆇🅿🆁🅴🆂🆂')
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+restaurantes = []
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-print('Você escolheu a opção {}!'.format(opcao_escolhida))
+def exibir_nome_do_programa():
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurantes')
+    print('4. Sair\n')
 
 def finalizar_app():
     os.system('cls')
-    # s.system('clear') no mac
-    print('========== Finalizando o app ==========')
+    print('Finalizando app')
 
-if opcao_escolhida == 1:
-    print('Cadastrar Restaurante')
-elif opcao_escolhida == 2:
-    print('Listar Restaurante')
-elif opcao_escolhida == 3:
-    print('Ativar restaurantes')
-else:
-    finalizar_app()
+def opcao_invalida():
+    print('Opçao invalida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
+def cadastrar_novo_restaurante():
+    os.system('cls')
+    print('Cadastro de novos Restaurantes\n')
+    nome_do_restaurante = input('Digite o nome do Restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print('O restaurante {} foi cadastrado!'.format(nome_do_restaurante))
+    input('Digite uma tecla para voltar ao menu principal...')
+    main()
+
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1:
+        cadastrar_novo_restaurante()
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurantes')
+    elif opcao_escolhida == 4:
+        finalizar_app()
+    else:
+        opcao_invalida()
+
+
+def main():
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
+
+
 
 
 
